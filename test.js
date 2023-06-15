3fb2711b8d190c35756b75381bad8202e642d740
ring03-cl <ring03.cl@gmail.com>
2023-06-15 21:28:38 +0300

    font+party colours+dropdown

---
+++ test.js
@@ -0,0 +1,6 @@
+var testData = [
+    {question: "Finland should accept fewer quota refugees from refugee camps."},
+    {question: "Labour immigration is needed to sustain the Finnish welfare society."},
+    {question: "Businesses must have the freedom to hire workers from outside the EU without an availability test."},
+    {question: "Finland must be able to deport immigrants who commit crimes more easily than at present."},
+];
