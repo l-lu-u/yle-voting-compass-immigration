83cf1d1d7961ded97de088a4896ec52b14452ac1
ring03-cl <ring03.cl@gmail.com>
2023-06-18 12:28:33 +0300

    colour coded candidate

--- js/candidate_plot.js
+++ js/candidate_plot.js
@@ -46,31 +46,62 @@ var svg = d3.select("#candidate_plot")
     .append("g")
         .attr("transform", "translate(0"+","+height/2+")");
 
-d3.json("./data/candidate-answers-2023.json").then(function(data){
-    
-    var simulation = d3.forceSimulation(data)
-        .force('charge', d3.forceManyBody().strength(1))
-        .force('x', d3.forceX().x(function(d) {
-            // console.log(d.fewer_quota_refugees + " " + xCenter[d.fewer_quota_refugees-1]);
-            return xCenter[d.fewer_quota_refugees-1];
-        }))
-        .force('collision', d3.forceCollide().radius(circleRadius))
-        .on('tick', ticked);
+plot(statements[0].statement);
 
-    function ticked() {
-        svg.selectAll('circle')
-            .data(data)
-            .join('circle')
-            .attr('r', circleRadius)
-            .attr('class', function(d){
-                return d.fewer_quota_refugees
-            })
-            .style('fill', "steelblue")
-            .attr('cx', function(d){
-                return d.x;
-            })
-            .attr('cy', function(d){
-                return d.y;
-            });
-    }
+//when the button is changed, update the plot
+d3.select("#selectButton").on("change", function(d) {
+    // recover the option that has been chosen
+    var selectedOption = d3.select(this).property("value")
+    // run the updateChart function with this selected option
+    plot(selectedOption)
 })
+
+function plot(selectedOption){
+    console.log(selectedOption);
+    svg.selectAll("*").remove();
+
+    d3.json("./data/candidate-answers-2023.json").then(function(data){
+
+        var simulation = d3.forceSimulation(data)
+            .force('charge', d3.forceManyBody().strength(1))
+            .force('x', d3.forceX().x(function(d) {
+                var scatterX;
+                switch(selectedOption) {
+                    case statements[0].statement:
+                        // console.log(d.fewer_quota_refugees + " " + xCenter[d.fewer_quota_refugees-1]);
+                        scatterX = xCenter[d.fewer_quota_refugees-1];
+                        break;
+                    case statements[1].statement:
+                        scatterX = xCenter[d.labour_immigration_needed-1];
+                        break;
+                    case statements[2].statement:
+                        scatterX = xCenter[d.no_availability_test-1];
+                        break;
+                    case statements[3].statement:
+                        scatterX = xCenter[d.deport_crimes_easily-1];
+                        break;
+                }
+                console.log(scatterX);
+                return scatterX;       
+            }))
+            .force('collision', d3.forceCollide().radius(circleRadius))
+            .on('tick', ticked);
+    
+        function ticked() {
+            svg.selectAll('circle')
+                .data(data)
+                .join('circle')
+                .attr('r', circleRadius)
+                .attr('class', function(d){
+                    return d.party;
+                })
+                .attr('cx', function(d){
+                    return d.x;
+                })
+                .attr('cy', function(d){
+                    return d.y;
+                });
+        }
+    })
+
+}
